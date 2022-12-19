1943f7eccc6cb11726f30d8443ce855870ef77b3
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-12-19 20:28:18 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-alert("hi"); // 웹페이지 콘솔 창에 새로고침을 하면 뜨게 하는 funtion
+// variable(변수 만드는법)
+
+
+const a = 5; // 변수 선언 방법 const는 상수를 의미
+const b = 4;
+const Myname = "leehyobin";
+
+console.log(a + b); // 콘솔로 메세지 보내기 (콘솔에 log, print(괄호안에는 숫자나 문자))
+console.log(a / b); // a는 5를 의미
+console.log(a * b); // ;;
+console.log("hello " + Myname);
+
+
